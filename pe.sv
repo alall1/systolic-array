6ad43f744d08555ab699e262af2c629d786985a3
Anuj Lall <alall1@ucsc.edu>
2026-08-23 10:15:51 -0700

    added sim_pe.cpp, needs fixing

--- pe.sv
+++ pe.sv
@@ -9,18 +9,21 @@ module pe #(
     input logic in_valid,                   // high when inputs are real data (high if PE is being used this cycle)
     output logic [DATA_WIDTH-1:0] out_a,    // registered copy of left neighbor input to send to right neighbor (passing along operand)
     output logic [DATA_WIDTH-1:0] out_b,    // registered copy of top neighbor input to send to bottom neighbor
-    output logic [ACC_WIDTH-1:0] acc        // running total sum, read at drain time. Output of the PE
+    output logic [ACC_WIDTH-1:0] acc,       // running total sum, read at drain time. Output of the PE
+    output logic out_valid                  // in_valid input into the next PEs; operand validity, not accumulator sum validity
 );
 
 always_ff @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
+    if (!rst_n || !in_valid) begin
         out_a <= '0;
         out_b <= '0;
         acc <= '0;
+        out_valid <= 1'b0;
     end else begin
         out_a <= in_a;
         out_b <= in_b;
-        acc <= acc + ($signed(in_a) * $signed(in_b));
+        acc <= acc + ($signed(in_a) * $signed(in_b));   // multiply accumulate
+        out_valid <= 1'b1;
     end
 end
 
--- sim_pe.cpp
+++ sim_pe.cpp
@@ -7,30 +7,82 @@ int main(int argc, char** argv) {
     // handing command-line args to Verilator so flags work
     Verilated::commandArgs(argc, argv);
 
-    // creating new instance of design; pointer "top"
-    Vpe* top = new Vpe;
+    // creating new instance of design; pointer "dut"
+    Vpe* dut = new Vpe;
 
     // ===== tracing setup for waveform file =====
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
-    top->trace(tfp, 99);
+    dut->trace(tfp, 99);
     tfp->open("pe.vcd");    // waveform output file
 
     // ===== loop variables =====
     vluint64_t time = 0;    // verilator's name for a 64-bit unsigned integer; used as a simulation timestamp that ticks upward
+    int case_count = 0;     // test case count
     int errors = 0;         // a counter for how many test cases fail
+    
+    int acc_sum = 0;        // accumulated sum of the 
+
+    /* ===== input & output pins =====
+    input clk,
+    input rst_n,  // active low
+    input [DATA_WIDTH-1:0] in_a,      // input from left neighbor
+    input [DATA_WIDTH-1:0] in_b,      // input from top neighbor
+    input in_valid,                   // high when inputs are real data (high if PE is being used this cycle)
+    output [DATA_WIDTH-1:0] out_a,    // registered copy of left neighbor input to send to right neighbor (passing along operand)
+    output [DATA_WIDTH-1:0] out_b,    // registered copy of top neighbor input to send to bottom neighbor
+    output [ACC_WIDTH-1:0] acc 
+    */
 
     // ===== loop functions =====
+    auto tick = [&]() {    // tick one clock cycle, one clk cycle = 2 waveform time units
+        top->clk = 0;
+        top->eval();
+        tfp->dump(sim_time++);  // reading current values & incrementing sim_time
+
+        top->clk = 1;
+        top->eval();
+        tfp->dump(sim_time++);  // reading current vals & incrementing sim_time
+    };
+
+    auto test_pe = [&](int A, int B) {
+        // set inputs (& OxFF because 8-bit inputs for now)
+        short_A = A & 0xFF;
+        short_B = B & 0xFF;
+        dut->in_a = short_A;
+        dut->in_b = short_B;
+
+        // calculating reference
+        acc_sum = acc_sum + (A*B);
+
+        tick();
+        
+        // comparing & writing
+        if (dut->out_a != short_A || dut->out_b != short_B || dut->acc != acc_sum) {
+            printf("ERROR: in_a=%d in_b=%d; got out_a=%d out_b=%d acc=%d; expected out_a=%d out_b=%d acc=%d\n",
+                    A, B, dut->out_a, dut->out_b, dut->acc, short_A, short_B, acc_sum);
+            errors++;
+        } else {
+            printf("OK: in_a=%d in_b=%d; got out_a=%d out_b=%d acc=%d\n",
+                    A, B, dut->out_a, dut->out_b, dut->acc);
+        }
+        case_count++;
+    };
 
     // ===== test loop =====
+    // resetting, active low
+    dut->rst_n = 0;
+    tick();
+    dut->rst_n = 1;
+    dut->in_valid = 1;
 
     // ===== cleanup =====
     tfp->close();   // close the waveform file so it is written to disk
-    delete top;     // free the memory allocated with "new"
+    delete dut;     // free the memory allocated with "new"
 
     // ===== final report =====
-    if (errors == 0) printf("\nAll test cases passed.\n");
-    else printf("\n%d test case(s) failed.\n", errors);
+    if (errors == 0) printf("\nAll %d test cases passed.\n", case_count);
+    else printf("\n%d test case(s) failed of %d total\n", errors, case_count);
 
     return errors ? 1 : 0; // return 0 if no errors, or 1 if there were.
 }
